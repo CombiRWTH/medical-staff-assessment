8dff3a1a1fc89c1fe8cf708023c6950b069b6c13
Ahmad Al Housseini <ahmad.al-housseini@rwth-aachen.de>
2024-11-26 02:25:32 +0100

    add stations endpoint

---
+++ backend/src/handle_stations.py
@@ -0,0 +1,33 @@
+"""Provide stations for the frontend to display  """
+
+from django.http import JsonResponse
+
+from ..models import (Station)
+
+
+def get_all_stations() -> list:
+    """Get all stations stored in the db.
+
+    Args:
+    none
+
+
+    Returns:
+        list: Stations.
+    """
+    return list(Station.objects.values())
+
+
+def handle_stations(request) -> JsonResponse:
+    """Endpoint to retrieve all current stations.
+
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        JsonResponse: The response containing the stations.
+    """
+    if request.method == 'GET':
+        return JsonResponse(get_all_stations(), safe=False)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
