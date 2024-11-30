85695055619ad3557369e0cca84898ed276dea33
Ahmad Al Housseini <ahmad.al-housseini@rwth-aachen.de>
2024-11-30 21:13:03 +0100

    change admin.py

--- backend/backend/admin.py
+++ backend/backend/admin.py
@@ -1,9 +1,13 @@
 """This file is used to register the models in the admin panel of the Django application."""
 from django.contrib import admin
+from django.urls.resolvers import URLPattern
 
-from .models import CareServiceCategory, CareServiceField, CareServiceOption, IsCareServiceUsed, DailyClassification, \
+from .models import (
+    CareServiceCategory, CareServiceField, CareServiceOption, IsCareServiceUsed, DailyClassification,
     Patient, Station, PatientTransfers, StationOccupancy, StationWorkloadDaily, StationWorkloadMonthly
+)
 
+# Register models without customization
 admin.site.register(CareServiceCategory)
 admin.site.register(CareServiceField)
 admin.site.register(CareServiceOption)
@@ -11,7 +15,26 @@ admin.site.register(IsCareServiceUsed)
 admin.site.register(DailyClassification)
 admin.site.register(Patient)
 admin.site.register(Station)
-admin.site.register(PatientTransfers)
 admin.site.register(StationOccupancy)
 admin.site.register(StationWorkloadDaily)
 admin.site.register(StationWorkloadMonthly)
+
+@admin.register(PatientTransfers)
+class PatientTransfersAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'admission_date', 'discharge_date', 'station_old', 'station_new', 'transferred_to_external')
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        TODO: make station_old readable if the patient is new (still dk how to implement it)
+        """
+        readonly_fields = super(PatientTransfersAdmin, self).get_readonly_fields(request, obj) or ()
+
+        if obj is not None: 
+            if obj.station_old is None:  
+                return readonly_fields + ('station_old',)
+            return readonly_fields
+
+        return readonly_fields
+
+  
+        
