6409ce7d8e4a888a595d06089e1594d16ac093f6
lennart123456 <lennart.huesing@rwth-aachen.de>
2024-12-29 15:17:16 +0100

    Flake8 Line too long

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,5 +25,9 @@ urlpatterns = [
         handle_patients.handle_current_station_of_patient,
         name='handle_current_station_of_patient'
     ),
-    path('import/patient/<str:date>/', handle_data_imports.handle_patient_data_import, name='handle_patient_data_import'),
+    path(
+        'import/patient/<str:date>/',
+        handle_data_imports.handle_patient_data_import,
+        name='handle_patient_data_import'
+    ),
 ]
