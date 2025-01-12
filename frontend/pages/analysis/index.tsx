c12041ec2b706410808912abe12710a372cc52c5
Presiyan <presiyan.prodanov@rwth-aachen.de>
2025-01-12 23:00:28 +0100

    lint

--- frontend/pages/analysis/index.tsx
+++ frontend/pages/analysis/index.tsx
@@ -34,7 +34,7 @@ export const AnalysisPage: NextPage = () => {
 
   const exportData = async () => {
     // Filter the stations
-    const filteredData = data.filter(station => selectedStations.includes(station.id))
+    const filteredData = data.filter((station: any) => selectedStations.includes(station.id))
 
     if (viewMode === 'daily') {
       await exportDailyAnalysis(filteredData as StationDaily[])
